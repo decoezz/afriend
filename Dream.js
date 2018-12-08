42f7c08d1087da64899bd978e790b775ce8a4f32
decoezz <45102309+decoezz@users.noreply.github.com>
2018-12-08 22:19:23 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(511615135350128643)
+setInterval(function() {
+channel.send(`a7a`);
+}, 25)
+})
+ 
+ 
+client.login(NDMwODAyNDA3ODMyODEzNTY4.DstZrg.sL2L2Lh8hoqjjV8ouPhQ_iVo02M);
